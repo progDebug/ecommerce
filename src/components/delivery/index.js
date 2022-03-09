a1899ad582f90caa4e32f2fd3f58b6e8d795d94d
JulioCesarAraujo <juliocesararaujosilva75@gmail.com>
2022-03-09 14:34:26 -0300

    adicionado formatação em jumbotron

--- src/components/delivery/index.js
+++ src/components/delivery/index.js
@@ -1,16 +1,17 @@
 import React from "react";
+import Deliverymen from "../../assets/images/deliveryman.jpg";
+import Cart from "../../assets/images/shopping-cart.jpg";
+import "./index.css";
 
 export default function Delivery() {
     return(
         <>
             <div className="container-delivery">
-                <image className="deliverymen">
-
-                </image>
+                <img src={Deliverymen} alt="Deliveryman" className="deliveryman"></img>
                 <div className="text">
                     <h3>How it works?</h3>
-                    <h1>We deliver anytime, everywhere in tblisi</h1>
-                    <p>If you food lover, our service is for you. Delicius food anytime, everywhere.</p>
+                    <h1>We deliver anytime,<br /> everywhere in tblisi</h1>
+                    <p>If you food lover, our service is for you.<br></br> Delicius food anytime, everywhere.</p>
                 </div>
                 <div className="buttons">
                 </div>
